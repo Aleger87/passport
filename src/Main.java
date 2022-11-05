78798337900b9530ac846cd9411f8eac7e759da4
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-11-06 02:29:50 +0300

    passport

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Passport passport = new Passport("0000 000000", "Иван","Иванов",  "01.01.2000" );
+        System.out.println(PassportMethod.passports);
+        Passport passpor2 = new Passport("0000 000000", "Иван","Иванов","Иванович", "01.01.2000" );
+        System.out.println(PassportMethod.passports);
+        Passport passport1 = new Passport("0000 000001", "Иван","Иванов", "01.01.2000" );
+        System.out.println(PassportMethod.passports);
 
+        searchPassport(passport.getPassportNumber());
+    }
+
+    private static void searchPassport(String passportNumber) {
+        System.out.println(PassportMethod.passports.get(passportNumber));
     }
 }
